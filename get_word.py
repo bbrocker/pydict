f23a2c7882c13a54fbaa3622006d2badc3cafdfd
Bill Brocker <williambrocker@yahoo.com>
2018-11-07 19:34:12 -0500

    added closeness check

--- get_word.py
+++ get_word.py
@@ -1,14 +1,21 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
-def translate(word):
-    word = word.lower()
-    if word in data:
-        return data[word]
+def translate(w):
+    w = w.lower()
+    if w in data:
+        return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
+        if yn == "Y":
+            return data[get_close_matches(w, data.keys())[0]]
+        else:
+            return w + " not found in dictionary."
     else:
-        return iword + " not found in dictionary."
+        return w + " not found in dictionary."
 
-iword = input("Enter a word: ")
+word = input("Enter a word: ")
 
-print(translate(iword))
+print(translate(word))
